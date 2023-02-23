3bcd933e79be38288955dddbd2cf8f94eb2cd6e1
avnish <avnishbdp@gmail.com>
2023-02-23 14:37:31 +0530

    fp06_270-cartpage-ui

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div>
       <AllRoutes/>
+      
     </div>
   );
 }
--- src/Pages/AllRoutes.jsx
+++ src/Pages/AllRoutes.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import {Route,Routes} from 'react-router-dom'
+import { Cart } from './Cart'
+
 import Homepage from './Homepage'
 const AllRoutes = () => {
   return (
     <Routes>
         <Route path='/' element={<Homepage/>}/>
+        <Route path='/cart' element={<Cart/>}/>
     </Routes>
   )
 }
