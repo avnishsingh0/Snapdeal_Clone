4a83e014cdbe2d97995eb0bc58f5b2080b4112bd
Soaib Akhtar <98397413+soaibakhtar@users.noreply.github.com>
2023-02-26 21:06:38 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -8,19 +8,32 @@ import AllRoutes from "./Pages/AllRoutes";
 import Login from "./Components/auth/Login";
 import Signup from "./Components/auth/Signup";
 import AuthDetails from "./Components/auth/AuthDetails";
+import { useEffect, useState } from "react";
+import { auth } from "./firebase";
 
 
 function App() {
+  const [userName, setUserName] = useState("");
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        setUserName(user.displayName);
+      } else setUserName("");
+    });
+  }, []);
+
+
   return (
     <div>
 
-      <Navbar/>
+      <Navbar name={userName}/>
       {/* <Login/> */}
       {/* <Signup/> */}
       {/* <AuthDetails/> */}
       {/* <MainPage/> */}
-      <MainPage/>
-      <LandingPage/>
+      {/* <MainPage/> */}
+      {/* <LandingPage/> */}
       <AllRoutes/>
     </div>
   );
